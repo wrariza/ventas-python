3b845e6d08908191b2f118ca4a35582de8c6b43a
William Ariza <william.ariza@almundo.com>
2019-06-20 12:17:14 -0500

    function

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-clients = 'pablo, ricardo'
+clients = 'pablo,ricardo,'
 
-if __name__ == "__main__":
-    clients += ', david'
+
+def create_client(client_name):
+    global clients
+    clients += client_name
+
+    _add_comma()
+
+
+def list_clients():
     print(clients)
 
+
+def _add_comma():
+    global clients
+
+    clients += ','
+
+
+if __name__ == "__main__":
+    list_clients()
+    create_client('carolina')
+    list_clients()
