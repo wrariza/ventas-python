90a4703bb62f29378111b10641da969038b36b43
William Ariza <william.ariza@almundo.com>
2019-06-20 13:46:25 -0500

    added and list client

--- main.py
+++ main.py
@@ -3,22 +3,40 @@ clients = 'pablo,ricardo,'
 
 def create_client(client_name):
     global clients
-    clients += client_name
-
-    _add_comma()
+    if client_name not in clients:
+        clients += client_name
+        list_clients()
+    else:
+        print('CLient already is in the client list')
 
 
 def list_clients():
     print(clients)
+    _add_comma
 
 
 def _add_comma():
     global clients
-
     clients += ','
 
 
+def _print_welcom():
+    print('WELCOME TO VENTAS')
+    print('*' * 55)
+    print('what would you live to do today')
+    print('[C] Create client')
+    print('[D] Delete client')
+
+
 if __name__ == "__main__":
-    list_clients()
-    create_client('carolina')
-    list_clients()
+    _print_welcom()
+    command = raw_input()
+
+    if command == 'C':
+        name_client = raw_input('Name of client? ')
+        create_client(name_client)
+
+    elif command == 'D':
+        pass
+    else:
+        print('Invalid command')
