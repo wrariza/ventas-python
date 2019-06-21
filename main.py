951f11ebd30353a82508e950351184e473dec77d
William Ariza <william.ariza@almundo.com>
2019-06-21 09:03:15 -0500

    create CRUD clients console

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 clients = 'pablo,ricardo,'
 
 
+def print_not_client_list():
+    print('Client not exist in the list clients')
+
+
+def get_name_client(message='Name of client?'):
+    return raw_input(message)
+
+
 def create_client(client_name):
     global clients
     if client_name not in clients:
         clients += client_name
         list_clients()
     else:
-        print('CLient already is in the client list')
+        print('client exist in the list')
+
+
+def _update_client(client_name, new_value_client):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name, new_value_client)
+        list_clients()
+    else:
+        print_not_client_list()
+
+
+def _delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name+',', '')
+        list_clients()
+    else:
+        print_not_client_list()
 
 
 def list_clients():
     print(clients)
-    _add_comma
+    _add_comma()
 
 
 def _add_comma():
@@ -26,17 +54,18 @@ def _print_welcom():
     print('what would you live to do today')
     print('[C] Create client')
     print('[D] Delete client')
+    print('[U] Update client')
 
 
 if __name__ == "__main__":
     _print_welcom()
-    command = raw_input()
+    command = raw_input().upper()
 
     if command == 'C':
-        name_client = raw_input('Name of client? ')
-        create_client(name_client)
-
+        create_client(get_name_client())
     elif command == 'D':
-        pass
+        _delete_client(get_name_client())
+    elif command == 'U':
+        _update_client(get_name_client('Name client do you want update ?'), get_name_client())
     else:
         print('Invalid command')
